16d664c5a9ef33a8e33bae1c1fcdc31327b0ee84
Thiago Suzuki <thiagosatoshi@gmail.com>
2022-03-08 19:54:35 +0000

    added book summary to connect to postgresql

--- comment_app.py
+++ comment_app.py
@@ -3,6 +3,7 @@ from forms import BookForm
 from forms import TeacherFeedbackForm
 from forms import NewBookForm
 import json
+import psycopg2
 
 
 
@@ -17,66 +18,83 @@ def index_page():
 
 
 
-file_connection = open("C:/code/comment_app/summaries.json", 'r')
+file_connection = open("C:\Git\evania\comment_app\summaries.json", 'r')
 summary = json.load(file_connection) #to  load the json file
 file_connection.close()
 
 summary=[]
 @app.route("/school/book_summary", methods = ["GET", "POST"])
 def book_summary():
+    print("THIAGO")
+
+    #establishing the connection
+    conn = psycopg2.connect(
+        database="postgres", 
+        user='digitalboost', 
+        password='digitalboost', 
+        host='127.0.0.1', 
+        port= '5432')
+    #Creating a cursor object using the cursor() method
+    cursor = conn.cursor()
+
+    # cursor.execute("select version()")
+    # db_version = cursor.fetchone()
+    # print(db_version)
+    
     book_form = BookForm(csrf_enabled=False)
     if book_form.validate_on_submit(): #validators make sure that the fields are not left blank
-        id = len(summary) + 1 # meaning you are ready to add 1 more item to your list
-        summary.append({
-      "book_title": book_form.book_title.data,
-      "book_summary": book_form.book_summary.data
-    })
-    file_connection = open("C:/code/comment_app/summaries.json", 'w') #makes sure that the data is written in the json file
-    json.dump(summary, file_connection) #dumping evrything that was appended in the json file
-    file_connection.close()
+        sql_command = f"insert into summary(book_title, book_summary) values ('{book_form.book_title.data}','{book_form.book_summary.data}')"
+        cursor.execute(sql_command)
+        conn.commit()
+        cursor.close()
+        conn.close()
+
     return render_template("book_summary.html",
     template_form = book_form, summary = summary)
 
 
-feedback=[]
-file_connection = open("C:/code/comment_app/feedback.json", 'r')
-feedback = json.load(file_connection)
-file_connection.close()
+# feedback=[]
+# file_connection = open("C:/Git/evania/comment_app/feedback.json", 'r')
+# feedback = json.load(file_connection)
+# file_connection.close()
+#
+#
+# @app.route("/school/teacher_feedback", methods = ["GET", "POST"])
+# def teacher_feedback():
+#     teacher_feedback_form = TeacherFeedbackForm(csrf_enabled=False)
+#     if teacher_feedback_form.validate_on_submit():
+#         id = len(feedback) + 1
+#         feedback.append({
+#       "student_name": teacher_feedback_form.student_name.data,
+#       "class_chosen":teacher_feedback_form.class_chosen.data,
+#       "teacher_feedback": teacher_feedback_form.teacher_feedback.data
+#     })
+#     file_connection = open("C:/code/comment_app/feedback.json", 'w')
+#     json.dump(feedback, file_connection)
+#     file_connection.close()
+#     return render_template("teacher_feedback.html",
+#     template_form = teacher_feedback_form, feedback = feedback)
+#
+# book=[]
 
-@app.route("/school/teacher_feedback", methods = ["GET", "POST"])
-def teacher_feedback():
-    teacher_feedback_form = TeacherFeedbackForm(csrf_enabled=False)
-    if teacher_feedback_form.validate_on_submit():
-        id = len(feedback) + 1
-        feedback.append({
-      "student_name": teacher_feedback_form.student_name.data,
-      "class_chosen":teacher_feedback_form.class_chosen.data,
-      "teacher_feedback": teacher_feedback_form.teacher_feedback.data
-    })
-    file_connection = open("C:/code/comment_app/feedback.json", 'w')
-    json.dump(feedback, file_connection)
-    file_connection.close()
-    return render_template("teacher_feedback.html",
-    template_form = teacher_feedback_form, feedback = feedback)
 
-book=[]
-@app.route("/school/new_book", methods = ["GET", "POST"])
-def new_book():
-    new_book_form = NewBookForm(csrf_enabled=False)
-    if new_book_form.validate_on_submit():
-        id = len(book) + 1
-        book.append({
-      "student_name": new_book_form.student_name.data,
-      "book_chosen":new_book_form.book_chosen.data
-    })
-    return render_template("new_book.html",
-    template_form = new_book_form, book = book)
-
-    #.book-summary-body {
-    #background-image: url("/static/main/childrensbook.jpg");
-    #background-size: cover;
-    #background-position: -245px -55px;
-    #background-repeat: no-repeat;
+# @app.route("/school/new_book", methods = ["GET", "POST"])
+# def new_book():
+#     new_book_form = NewBookForm(csrf_enabled=False)
+#     if new_book_form.validate_on_submit():
+#         id = len(book) + 1
+#         book.append({
+#       "student_name": new_book_form.student_name.data,
+#       "book_chosen":new_book_form.book_chosen.data
+#     })
+#     return render_template("new_book.html",
+#     template_form = new_book_form, book = book)
+#
+#     #.book-summary-body {
+#     #background-image: url("/static/main/childrensbook.jpg");
+#     #background-size: cover;
+#     #background-position: -245px -55px;
+#     #background-repeat: no-repeat;
 
 
 
